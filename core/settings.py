52d102bd916ac7d1e4dc3dd02ac1424c02624c5f
Souleymane.ndiaye <souleymane.ndiaye@vnb-it.fr>
2025-05-11 01:33:31 +0000

    Update DATABASE_URL in settings.py to use external PostgreSQL connection for improved accessibility.

--- core/settings.py
+++ core/settings.py
@@ -137,7 +137,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-DATABASE_URL = "postgresql://postgres:AGoHKPfINFzJWzwankCiGExwGtEpRyoO@postgres.railway.internal:5432/railway"
+DATABASE_URL = "postgresql://postgres:AGoHKPfINFzJWzwankCiGExwGtEpRyoO@switchback.proxy.rlwy.net:51653/railway"
 DATABASES = {
     
     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
